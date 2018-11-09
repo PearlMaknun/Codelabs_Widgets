b06dbde0afeb9b3de2ef14082669e78fdf907eb0
PearlMaknun <almaknunlulu6@gmail.com>
2018-11-09 07:11:14 +0700

    Part 1

---
+++ app/src/main/java/com/pearlmaknun/latihan_androidwidgets/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pearlmaknun.latihan_androidwidgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
